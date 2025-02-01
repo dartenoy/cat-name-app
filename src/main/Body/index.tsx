9ff1d383cc6aa9da0c86cd8db69deb6aba49bdb6
dartenoy <tomasrepsys0@gmail.com>
2025-02-01 20:14:26 +0200

    Removed console log

--- src/main/Body/index.tsx
+++ src/main/Body/index.tsx
@@ -14,7 +14,6 @@ const Body: React.FC = () => {
   const [dialogOpen, setDialogOpen] = useState(false);
 
   const clickHandler = () => {
-    console.log("Button clicked");
     setDialogOpen(true);
   };
 
