0c181dbab79f60f90c5959b0af0ee61ad7468653
dartenoy <tomasrepsys0@gmail.com>
2025-02-02 22:52:35 +0200

    darken paragraph color

--- src/components/text/Paragraphs.tsx
+++ src/components/text/Paragraphs.tsx
@@ -4,9 +4,9 @@ interface Paragraph {
 }
 
 export const StyledParagraph: React.FC<Paragraph> = ({ text }) => {
-  return <p className="mb-5 text-base text-gray-500 ">{text}</p>;
+  return <p className="mb-5 text-base text-gray-700">{text}</p>;
 };
 
 export const StyledSmalledParagraph: React.FC<Paragraph> = ({ text }) => {
-  return <p className="mb-5 text-sm text-gray-500 ">{text}</p>;
+  return <p className="mb-5 text-sm text-gray-700">{text}</p>;
 };
