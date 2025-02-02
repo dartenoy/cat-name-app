d5ea6144c573d41e4e735cb27b1addabec23f2fa
dartenoy <tomasrepsys0@gmail.com>
2025-02-02 23:07:06 +0200

    comments update

--- src/hooks/useScrollControl.tsx
+++ src/hooks/useScrollControl.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useRef } from "react";
 
+//// how close to be next to bottom in px, to trigger item load
 const tolerance = 5;
 
 const useScrollControl = (
@@ -13,7 +14,8 @@ const useScrollControl = (
   const handleScroll = () => {
     /// scroll handler on cooldown
     if (cooldownRef.current === true) return;
-    ///// checking is scroll is down
+
+    //// Loading more items when near very bottom of scroll
     if (
       scrollRef.current &&
       scrollRef.current.scrollTop + scrollRef.current.clientHeight >=
